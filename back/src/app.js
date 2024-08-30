fb07a29a5366ffc4bee8f65f14727d270d6d6614
ACastilloSantiago <santiagorodrigec@gmail.com>
2024-08-29 22:19:59 -0300

    feat: :bug: add cors and try routes

--- back/src/app.js
+++ back/src/app.js
@@ -2,22 +2,24 @@ const express = require("express");
 const server = express();
 
 const morgan = require("morgan");
+const cors = require('cors');
 
 const router = require("./routes/index");
 
+server.use(cors());
 server.use(express.json());
 server.use(morgan("dev"));
 
-server.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header(
-    "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
-  );
-  res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
-  next();
-});
+// server.use((req, res, next) => {
+//   res.header("Access-Control-Allow-Origin", "*");
+//   res.header("Access-Control-Allow-Credentials", "true");
+//   res.header(
+//     "Access-Control-Allow-Headers",
+//     "Origin, X-Requested-With, Content-Type, Accept"
+//   );
+//   res.header("Access-Control-Allow-Methods", "GET, POST, OPTIONS, PUT, DELETE");
+//   next();
+// });
 
 server.use("/rickandmorty", router);
 
